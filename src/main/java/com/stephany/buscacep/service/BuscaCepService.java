4579614e881d81780020def8464046edecbeda12
Stephany Rodrigues <stephany0303@gmail.com>
2021-05-24 00:11:52 -0300

    Adiciona logica de busca e adicao de zero ao final

--- src/main/java/com/stephany/buscacep/service/BuscaCepService.java
+++ src/main/java/com/stephany/buscacep/service/BuscaCepService.java
@@ -5,33 +5,58 @@ import com.stephany.buscacep.response.Endereco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import static com.stephany.buscacep.mock.EnderecoMock.enderecosMocados;
+
 @Service
 @Slf4j
 public class BuscaCepService {
 
     private static final int MENOR_CEP_VALIDO = 1000000;
+    private static final int TAMANHO_MAX = 8;
 
-    public Endereco buscaCep(String cep) throws CepInvalidoException{
+    public Endereco buscaCep(String cep) throws CepInvalidoException {
+        StringBuilder novoCep = new StringBuilder(cep);
+        Integer count = 1;
+        Endereco endereco;
 
-        isCepValido(cep);
+        while (count <= TAMANHO_MAX) {
 
-        // lógica para busca de cep
+            log.info("m=buscaCep, stage=tentativa, cep={}", novoCep, count);
+            isCepValido(novoCep);
+            endereco = enderecosMocados.get(novoCep.toString());
 
-        return Endereco.builder()
-                .rua("Rua das Flores")
-                .bairro("Algum Bairro")
-                .cidade("Recife")
-                .estado("Pernambuco")
-                .build();
+            if(endereco != null){
+                log.info("m=buscaCep, stage=success, cep-novo={}, cep-original={}, endereco={}", novoCep, cep, endereco);
+                return endereco;
+            }
+
+            count = adicionaZeroNoFinal(novoCep, count);
+
+        }
+
+        throw new CepInvalidoException("CEP não existe");
     }
 
-    public void isCepValido(String cep) throws CepInvalidoException{
-        if(cep.isEmpty()){
+    public void isCepValido(StringBuilder cep) throws CepInvalidoException {
+        if (cep.isEmpty()) {
             throw new CepInvalidoException("CEP vazio ou nulo");
-        } else if(!cep.matches("\\d{8}")){
+        } else if (!cep.toString().matches("\\d{8}")) {
             throw new CepInvalidoException("Formato de CEP inválido");
-        } else if (Integer.parseInt(cep) < MENOR_CEP_VALIDO) {
+        } else if (Integer.parseInt(cep.toString()) < MENOR_CEP_VALIDO) {
             throw new CepInvalidoException("CEP não existe");
         }
+    }
+
+    public int adicionaZeroNoFinal(StringBuilder cep, Integer count) {
+        while(cep.charAt(TAMANHO_MAX - count) == '0'){
+            if(count < TAMANHO_MAX){
+                count++;
+            } else {
+                return count;
+            }
+        }
+        cep.deleteCharAt(TAMANHO_MAX - count).append("0");
+        count++;
+        return count;
     }
 }
