8ac4f939efdd06aedf384248676f4ef37913941c
Stephany Rodrigues <stephany0303@gmail.com>
2021-05-25 23:38:47 -0300

    Altera requisição de query parameter para path variable

--- src/main/java/com/stephany/buscacep/controller/BuscaCepController.java
+++ src/main/java/com/stephany/buscacep/controller/BuscaCepController.java
@@ -8,9 +8,10 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,15 +27,19 @@ public class BuscaCepController {
     @ApiOperation(value = "Retorna o endereço relacionado ao CEP inserido")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Retorna o endereço correspondente ao CEP", response = Endereco.class),
-            @ApiResponse(code = 422, message = "CEP inválido")
+            @ApiResponse(code = 422, message = "CEP inválido", response = ApiError.class)
     })
-    @GetMapping(value = "/busca-cep")
-    public ResponseEntity<Object> buscaCep(@RequestParam(value = "cep") String cep) {
+    @GetMapping(value = "/busca-cep/{cep}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> buscaCep(@PathVariable("cep") String cep) {
         try{
-            return ResponseEntity.ok().body(buscaCepService.buscaCep(cep));
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(buscaCepService.buscaCep(cep));
         } catch (CepInvalidoException e){
             log.error("m=get-buscacep, stage=error, msg=cep inválido, cep={}, message={}", cep, e.getMessage());
-            return ResponseEntity.unprocessableEntity().body(new ApiError("CEP inválido"));
+            return ResponseEntity.unprocessableEntity()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(new ApiError("CEP inválido"));
         }
     }
 }
